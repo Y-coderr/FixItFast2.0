b21210190b16c2b49906bdfffc401eb07b2612c5
Yash Kadam <kadamyash6112@gmail.com>
2025-10-04 22:23:55 +0530

    Save local changes before pull

--- backend/src/main/java/com/example/firebase/springbootfirebasedemo/config/ThymeleafConfig.java
+++ backend/src/main/java/com/example/firebase/springbootfirebasedemo/config/ThymeleafConfig.java
@@ -1,6 +1,6 @@
 package com.example.firebase.springbootfirebasedemo.config;
 
-import org.springframework.context.annotation.Bean;
+import  org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
